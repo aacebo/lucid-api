4027856a8ea5c6fd15ec4a8d74071031c714e931
aacebo <44811138+aacebo@users.noreply.github.com>
2020-06-13 12:45:01 -0400

    Initial commit

---
+++ src/main/java/com/lucid/api/LucidApplication.java
@@ -0,0 +1,13 @@
+package com.lucid.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucidApplication.class, args);
+	}
+
+}
